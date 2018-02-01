7c58a6c2051e83e0ec267719babc149e840ac554
kaviyavsb <31506458+kaviyavsb@users.noreply.github.com>
2018-02-01 14:38:50 +0530

    Create k in array.c

---
+++ k in array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+	int k,a[5],i;
+	scanf("%d",&k);
+	for(i=1;i<=100;i++)
+	scanf("%d",&a[i]);
+	for(i=1;i<=100;i++)
+	if(a[i]==k)
+	printf("yes");
+	return 0;
+}
