b5765e184a6b6c0f70fc4c8238e4b286a521c852
kaviyavsb <31506458+kaviyavsb@users.noreply.github.com>
2018-02-06 14:50:46 +0530

    Update less than k numbers.c

--- less than k numbers.c
+++ less than k numbers.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 int main(void) {
- int a[10],n,k,i; 
+ int a[10],n,k,i,j,temp; 
  scanf("%d %d",&n,&k);
  for(i=1;i<=n;i++)
  scanf("%d",&a[i]);
  for(i=1;i<=n;i++)
  {
- 	if(a[i]<k)
- 	printf("the numbers less than k are %d\n",a[i]);
+ for(j=i+1;j<=n;j++)
+ {
+  if(a[i]>a[j])
+   {
+    temp=a[i];
+    a[i]=a[j];
+    a[j]=temp;
+    }
+  }
+ if(a[i]<k)
+ printf("%d\t",a[i]);
  }
-	return 0;
+ return 0;
 }
